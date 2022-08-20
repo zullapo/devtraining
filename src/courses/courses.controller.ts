7d860a9f530d8c85add4b6d257638762aff59a85
zullapo <pcluz62@gmail.com>
2022-08-20 18:28:22 -0300

    checkpoint: aula 17

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -10,32 +10,36 @@ import {
   Post,
   Res,
 } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './dtos/create-course.dto';
+import { UpdateCourseDto } from './dtos/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `Curso ${id}`;
-  }
+  constructor(private readonly coursesService: CoursesService) {}
 
   @Get()
-  findAll(@Res() response) {
-    return response.status(200).send('Listagem de cursos');
+  findAll() {
+    return this.coursesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coursesService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
-  create(@Body('name') body) {
-    return body;
+  create(@Body() createCourseDto: CreateCourseDto) {
+    return this.coursesService.create(createCourseDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body('name') body) {
-    return `Atualização do curso ${id}`;
+  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+    return this.coursesService.update(id, updateCourseDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `Exclusão do curso ${id}`;
+    return this.coursesService.remove(id);
   }
 }
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  private courses: Course[] = [];
+
+  constructor() {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Fundamentos NestJS',
+        description: 'Fundamentos NestJS',
+        tags: ['node.js', 'nestjs', 'typescript', 'javascript'],
+      },
+    ];
+  }
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    const course = this.courses.find((course) => course.id === parseInt(id));
+    if (!course) {
+      throw new HttpException(
+        `Course of id ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return course;
+  }
+
+  create(createCourseDto: any) {
+    this.courses.push(createCourseDto);
+    return createCourseDto;
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id == parseInt(id),
+    );
+    if (indexCourse >= 0) {
+      this.courses[indexCourse] = updateCourseDto;
+    }
+  }
+
+  remove(id: string) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id == parseInt(id),
+    );
+    if (indexCourse >= 0) {
+      this.courses.splice(indexCourse, 1);
+    }
+  }
+}
